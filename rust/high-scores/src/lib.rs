5bc09ad4bccc1a52a06541c7b03dc8a996e4b802
0x2biimi <0x2biimi@gmail.com>
2025-07-06 23:57:36 +0900

    high-scores

---
+++ rust/high-scores/src/lib.rs
@@ -0,0 +1,35 @@
+#[derive(Debug)]
+pub struct HighScores {
+    scores: Vec<u32>,
+}
+
+impl HighScores {
+    pub fn new(scores: &[u32]) -> Self {
+        Self {
+            scores: scores.to_vec(),
+        }
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        &self.scores
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.scores.last().copied()
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        self.scores.iter().max().copied()
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        let mut a = self.scores.clone();
+        a.sort();
+        a.reverse();
+        if a.len() < 3 {
+            a
+        } else {
+            a[..3].to_vec()
+        }
+    }
+}
