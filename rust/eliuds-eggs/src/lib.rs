62d6de95bfd2a08bc5b8a1f0490b899c41e41f11
0x2biimi <0x2biimi@gmail.com>
2025-07-10 12:39:34 +0900

    eliuds-eggs-2

--- rust/eliuds-eggs/src/lib.rs
+++ rust/eliuds-eggs/src/lib.rs
@@ -1,3 +1,15 @@
 pub fn egg_count(display_value: u32) -> usize {
-    display_value.count_ones() as usize
+    let mut value = display_value;
+    let mut count: usize = 0;
+
+    loop {
+        if value == 0 {
+            break;
+        }
+        if value % 2 != 0 {
+            count += 1;
+        }
+        value >>= 1;
+    }
+    count
 }
